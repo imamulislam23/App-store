0f63d42504e3bbf897cc89c4761c3b74737dcc88
imamulislam23 <imamulislam672@gmail.com>
2025-10-23 20:00:10 +0600

    first commit

---
+++ src/Routes/Routes.jsx
@@ -0,0 +1,13 @@
+import { createBrowserRouter } from "react-router";
+import MainLayout from "../MainLayout/MainLayout";
+
+
+const  router = createBrowserRouter([
+    {
+        path:'/',
+        element: <MainLayout></MainLayout>
+    }
+]
+)
+
+export default router
